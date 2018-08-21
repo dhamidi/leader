1a7be74de8cf608c729205a5ef52d5ecaac85f6b
Dario Hamidi <dario@gowriteco.de>
2018-08-21 22:38:00 +0300

    Support Ctrl+V for literal insertion in fish
    Also fixes the installation of key bindings when running leader init
    in ~/.config/fish/config.fish.

--- assets/leader.fish.sh
+++ assets/leader.fish.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/fish
 
+function do-nothing
+end
+
 function run-leader
    set --export FISH_INPUT (commandline)
    set --export FISH_POINT (commandline -C)
@@ -11,4 +14,8 @@ function run-leader
    end
 end
 
-bind \\ run-leader
+function fish_user_key_bindings
+  bind \\ run-leader
+  bind -M literal --sets-mode default '' self-insert
+  bind -M default --sets-mode literal \cv do-nothing
+end
